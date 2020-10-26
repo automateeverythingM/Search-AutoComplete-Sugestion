d1d7d934d12152d978db3a886b8a0c08ec4ac42f
automateeverythingM <markospavicbg@gmail.com>
2020-10-26 20:40:52 +0100

    redux dev tools added

--- src/components/SearchAutoCompleteStyledComp/store/configStore.js
+++ src/components/SearchAutoCompleteStyledComp/store/configStore.js
@@ -2,7 +2,10 @@ import React from "react";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import searchReducer from "./MainSearch/mainSearchReducer";
-const store = createStore(searchReducer);
+const store = createStore(
+    searchReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default function WrapperProvider({ children }) {
     return <Provider store={store}>{children}</Provider>;
